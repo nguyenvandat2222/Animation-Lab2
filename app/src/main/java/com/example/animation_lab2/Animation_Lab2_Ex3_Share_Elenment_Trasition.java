ab97d65723c5f2e638e1f7ea9f63d0b4844110af
Dat <nguyenvandat.2222.vn@gmail.com>
2021-09-30 11:16:06 +0700

    first commit

---
+++ app/src/main/java/com/example/animation_lab2/Animation_Lab2_Ex3_Share_Elenment_Trasition.java
@@ -0,0 +1,14 @@
+package com.example.animation_lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Animation_Lab2_Ex3_Share_Elenment_Trasition extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animation_lab2_ex3_share_elenment_trasition);
+    }
+}
